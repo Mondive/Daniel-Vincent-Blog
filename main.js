753fdfd138dfe1876bd9f4fe900a88af5785ae21
Daniel Vincent <146905691+Mondive@users.noreply.github.com>
2024-11-24 01:22:04 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+console.log('Hello World!');
+
+
+const year = new Date().getFullYear();
+      document.addEventListener("DOMContentLoaded", function() {
+            document.getElementById("copyright-year").textContent = year;
+        });
